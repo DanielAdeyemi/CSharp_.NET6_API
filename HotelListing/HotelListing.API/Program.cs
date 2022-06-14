7b697ed76ad4df3dcb8a6f9ed9325944acabca98
DanielAdeyemi <adeyemidany@gmail.com>
2022-06-14 16:50:11 -0700

    add cors policies into program file

--- HotelListing/HotelListing.API/Program.cs
+++ HotelListing/HotelListing.API/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+} );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +22,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
